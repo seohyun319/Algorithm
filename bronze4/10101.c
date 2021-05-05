590ae97642cb7edb77e9c7ccd6f6adae1d7e4622
seohyun319 <tjguselly0319@gmail.com>
2021-05-05 16:13:17 +0900

    삼각형 외우기

---
+++ bronze4/10101.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int main() {
+	int a, b, c;
+	
+	scanf("%d", &a);
+	scanf("%d", &b);
+	scanf("%d", &c);
+
+	if (a == 60 && b == 60 && c==60) printf("Equilateral");
+	else if (a + b + c != 180) printf("Error");
+	else if (a == b || b == c || a == c) printf("Isosceles");
+	else if (a != b && b != c && a != c) printf("Scalene");
+
+
+	return 0;
+}
+	
