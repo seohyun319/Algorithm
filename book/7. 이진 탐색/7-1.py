5596e04a3e7cb799aa413f656655a60221acb2fd
seohyun319 <tjguselly0319@gmail.com>
2022-01-19 23:56:12 +0900

    C7 - 순차 탐색

---
+++ book/7. 이진 탐색/7-1.py
@@ -0,0 +1,19 @@
+# C7. 이진 탐색 - 순차 탐색 소스 코드
+
+def sequential_search(n, target, array):
+    #각 원소를 하나씩 확인하며
+    for i in range(n):
+        # 현재의 원소가 찾고자하는 원소와 동일한 경우
+        if array[i] == target:
+            return i + 1 #현재의 인덱스 번호 반환 (0부터 시작하니까 1 더해서 원하는 값으로 표기)
+
+print("생성할 원소 개수를 입력한 다음 한 칸 띄고 찾을 문자열을 입력하세요.")
+input_data = input().split()
+n = int(input_data[0]) #원소의 개수 
+target = input_data[1] #찾고자 하는 문자열
+
+print("앞서 적은 원소 개수만큼 문자열을 입력하세요. 구분은 띄어쓰기 한 칸으로 합니다.")
+array = input().split()
+
+# 순차 탐색 수행 결과 출력
+print(sequential_search(n, target, array))
