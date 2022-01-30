146ed31ef47af7f6d035177557fa3ab1a9ab5ddf
seohyun319 <tjguselly0319@gmail.com>
2022-01-30 23:55:29 +0900

    silver4 주유소

---
+++ ICPC/algo_camp/4_greedy/13305.py
@@ -0,0 +1,20 @@
+# silver4-13305. 주유소
+
+import sys
+put = sys.stdin.readline
+
+# 각 도시에 있는 주유소의 기름 가격과, 각 도시를 연결하는 도로의 길이를 입력으로 받아 제일 왼쪽 도시에서 제일 오른쪽 도시로 이동하는 최소의 비용을 계산
+
+n = int(put()) #도시 개수
+length = list(map(int, put().split())) #도로의 길이
+price = list(map(int, put().split())) #도시의 리터당 가격
+
+# (i-1)번째에서 i번째 도시로 가는 거리에 소모되는 기름은 
+# 1~(i-1)번째 주유소 중 가장  가격이 싼 도시의 주유소 고르면 됨
+sum = 0
+minPrice = price[0]
+for i in range(n-1):
+  if minPrice > price[i]:
+    minPrice = length[i]
+  sum += minPrice*length[i]  
+print(sum)
